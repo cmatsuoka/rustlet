325a0a330769681a47339711407c79008f8929f0
Claudio Matsuoka <cmatsuoka@gmail.com>
2017-12-04 10:45:20 -0200

    Add unit tests for smushing rules
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@cits.br>

--- src/rustlet/mod.rs
+++ src/rustlet/mod.rs
@@ -40,6 +40,15 @@ macro_rules! cmp_return {
     }
 }
 
+macro_rules! apply_rule {
+    ( $cmd:expr ) => {
+        match $cmd {
+            Some(val) => return Some(val),
+            None      => {},
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Smusher<'a> {
     prev_width: isize,
@@ -98,19 +107,22 @@ impl<'a> Smusher<'a> {
             return None
         }
 
-        let hardblank = self.font.hardblank;
+        Self::do_smush(l, r, self.font.hardblank, self.right2left, self.mode)
+    }
+
+    fn do_smush(l: char, r: char, hardblank: char, right2left: bool, mode: u32) -> Option<char> {
 
         // Universal smushing simply overrides the sub-character from the earlier
         // FIGcharacter with the sub-character from the later FIGcharacter. This
         // produces an "overlapping" effect with some FIGfonts, wherin the latter
         // FIGcharacter may appear to be "in front".
-        if self.mode == 0 {
+        if mode == 0 {
             // Ensure overlapping preference to visible characters
             cmp_return_other!(hardblank, l, r);
 
             // Ensures that the dominant (foreground) fig-character for overlapping is
             // the latter in the user's text, not necessarily the rightmost character
-            if self.right2left {
+            if right2left {
                 return Some(l)
             }
 
@@ -119,62 +131,169 @@ impl<'a> Smusher<'a> {
 
         // Rule 6: HARDBLANK SMUSHING (code value 32)
         // Smushes two hardblanks together, replacing them with a single hardblank.
-        if l == hardblank || r == hardblank {
-            if self.mode & figfont::SMUSH_HARDBLANK != 0 {
-                return Some(l)
+        if l == hardblank && r == hardblank {
+            if mode & figfont::SMUSH_HARDBLANK != 0 {
+                return Some(l);
             } else {
-                return None
+                return None;
             }
         }
 
-        // Rule 1: EQUAL CHARACTER SMUSHING (code value 1)
-        // Two sub-characters are smushed into a single sub-character if they are the
-        // same (except hardblanks). 
-        if self.mode & figfont::SMUSH_EQUAL != 0 {
-            if l == r {
-                return Some(l)
+        apply_rule!(smush_rule_1(l, r, mode));
+        apply_rule!(smush_rule_2(l, r, mode));
+        apply_rule!(smush_rule_3(l, r, mode));
+        apply_rule!(smush_rule_4(l, r, mode));
+        apply_rule!(smush_rule_5(l, r, mode));
+
+        None
+    }
+}
+
+// Rule 1: EQUAL CHARACTER SMUSHING (code value 1)
+// Two sub-characters are smushed into a single sub-character if they are the same (except
+// hardblanks). 
+fn smush_rule_1(l: char, r: char, mode: u32) -> Option<char> {
+    if mode & figfont::SMUSH_EQUAL != 0 {
+        if l == r {
+            return Some(l)
+        }
+    }
+
+    None
+}
+
+// Rule 2: UNDERSCORE SMUSHING (code value 2)
+// An underscore ("_") will be replaced by any of: "|", "/", "\", "[", "]", "{", "}", "(",
+// ")", "<" or ">".
+fn smush_rule_2(l: char, r: char, mode: u32) -> Option<char> {
+    if mode & figfont::SMUSH_UNDERLINE != 0 {
+        find_return_latter!("_", r"|/\[]{}()<>", l, r);
+    }
+
+    None
+}
+
+// Rule 3: HIERARCHY SMUSHING (code value 4)
+// A hierarchy of six classes is used: "|", "/\", "[]", "{}", "()", and "<>". When two
+// smushing sub-characters are from different classes, the one from the latter class will
+// be used.
+fn smush_rule_3(l: char, r: char, mode: u32) -> Option<char> {
+    if mode & figfont::SMUSH_HIERARCHY != 0 {
+        find_return_latter!("|", r"/\[]{}()<>", l, r);
+        find_return_latter!(r"/\", "[]{}()<>", l, r);
+        find_return_latter!("[]", "{}()<>", l, r);
+        find_return_latter!("{}", "()<>", l, r);
+        find_return_latter!("()", "<>", l, r);
+    }
+
+    None
+}
+
+
+// Rule 4: OPPOSITE PAIR SMUSHING (code value 8)
+// Smushes opposing brackets ("[]" or "]["), braces ("{}" or "}{") and parentheses ("()"
+// or ")(") together, replacing any such pair with a vertical bar ("|").
+fn smush_rule_4(l: char, r: char, mode: u32) -> Option<char> {
+    if mode & figfont::SMUSH_PAIR != 0 {
+        cmp_any_return!('[', ']', l, r, '|');
+        cmp_any_return!('{', '}', l, r, '|');
+        cmp_any_return!('(', ')', l, r, '|');
+    }
+
+    None
+}
+
+// Rule 5: BIG X SMUSHING (code value 16)
+// Smushes "/\" into "|", "\/" into "Y", and "><" into "X". Note that "<>" is not smushed
+// in any way by this rule. The name "BIG X" is historical; originally all three pairs
+// were smushed into "X".
+fn smush_rule_5(l: char, r: char, mode: u32) -> Option<char> {
+    if mode & figfont::SMUSH_BIGX != 0 {
+        cmp_return!('/', '\\', l, r, '|');
+        cmp_return!('\\', '/', l, r, 'Y');
+        cmp_return!('>', '<', l, r, 'X');
+    }
+
+    None
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rule_1() {
+        assert_eq!(smush_rule_1('-', '-', 0), None);
+        assert_eq!(smush_rule_1('-', 'x', 0), None);
+        assert_eq!(smush_rule_1('-', '-', figfont::SMUSH_EQUAL), Some('-'));
+        assert_eq!(smush_rule_1('-', 'x', figfont::SMUSH_EQUAL), None);
+    }
+
+    #[test]
+    fn test_rule_2() {
+        for x in [ '|', '/', '\\', '[', ']', '{', '}', '(', ')', '<', '>' ].iter() {
+            assert_eq!(smush_rule_2('_', *x, 0), None);
+            assert_eq!(smush_rule_2('_', *x, figfont::SMUSH_UNDERLINE), Some(*x));
+        }
+        assert_eq!(smush_rule_2('_', 'x', 0), None);
+        assert_eq!(smush_rule_2('_', 'x', figfont::SMUSH_UNDERLINE), None);
+    }
+
+    #[test]
+    fn test_rule_3() {
+        let list = [ '|', '/', '\\', '[', ']', '{', '}', '(', ')', '<', '>' ];
+
+        for x in list.iter() {
+            for y in list.iter() {
+                assert_eq!(smush_rule_3(*x, *y, 0), None);
             }
         }
 
-        // Rule 2: UNDERSCORE SMUSHING (code value 2)
-        // An underscore ("_") will be replaced by any of: "|", "/", "\", "[", "]",
-        // "{", "}", "(", ")", "<" or ">".
-        if self.mode & figfont::SMUSH_UNDERLINE != 0 {
-            find_return_latter!("_", r"|/\[]{}()<>", l, r);
+        assert_eq!(smush_rule_3('|', '|', figfont::SMUSH_HIERARCHY), None);
+        for x in list[1..].iter() {
+            assert_eq!(smush_rule_3('|', *x, figfont::SMUSH_HIERARCHY), Some(*x));
         }
-
-        // Rule 3: HIERARCHY SMUSHING (code value 4)
-        // A hierarchy of six classes is used: "|", "/\", "[]", "{}", "()", and "<>".
-        // When two smushing sub-characters are from different classes, the one from
-        // the latter class will be used.
-        if self.mode & figfont::SMUSH_HIERARCHY != 0 {
-            find_return_latter!("|", r"|/\[]{}()<>", l, r);
-            find_return_latter!(r"/\", r"|/\[]{}()<>", l, r);
-            find_return_latter!("[]", "{}()<>", l, r);
-            find_return_latter!("{}", "()<>", l, r);
-            find_return_latter!("()", "<>", l, r);
+        for i in 0..4 {
+            let idx = 3 + i*2;
+            for x in list[idx..idx+1].iter() {
+                for y in list[idx+2..].iter() {
+                    assert_eq!(smush_rule_3(*x, *y, figfont::SMUSH_HIERARCHY), Some(*y));
+                    assert_eq!(smush_rule_3(*y, *x, figfont::SMUSH_HIERARCHY), Some(*y));
+                }
+            }
         }
+    }
 
-        // Rule 4: OPPOSITE PAIR SMUSHING (code value 8)
-        // Smushes opposing brackets ("[]" or "]["), braces ("{}" or "}{") and parentheses
-        // ("()" or ")(") together, replacing any such pair with a vertical bar ("|").
-        if self.mode & figfont::SMUSH_PAIR != 0 {
-            cmp_any_return!('[', ']', l, r, '|');
-            cmp_any_return!('[', ']', l, r, '|');
-            cmp_any_return!('{', '}', l, r, '|');
-            cmp_any_return!('(', '}', l, r, '|');
-        }
+    #[test]
+    fn test_rule_4() {
+        assert_eq!(smush_rule_4('[', ']', 0), None);
+        assert_eq!(smush_rule_4(']', '[', 0), None);
+        assert_eq!(smush_rule_4('{', '}', 0), None);
+        assert_eq!(smush_rule_4('}', '{', 0), None);
+        assert_eq!(smush_rule_4('(', ')', 0), None);
+        assert_eq!(smush_rule_4(')', '(', 0), None);
+        assert_eq!(smush_rule_4('[', ']', figfont::SMUSH_PAIR), Some('|'));
+        assert_eq!(smush_rule_4(']', '[', figfont::SMUSH_PAIR), Some('|'));
+        assert_eq!(smush_rule_4('{', '}', figfont::SMUSH_PAIR), Some('|'));
+        assert_eq!(smush_rule_4('}', '{', figfont::SMUSH_PAIR), Some('|'));
+        assert_eq!(smush_rule_4('(', ')', figfont::SMUSH_PAIR), Some('|'));
+        assert_eq!(smush_rule_4(')', '(', figfont::SMUSH_PAIR), Some('|'));
+        assert_eq!(smush_rule_4('(', 'x', figfont::SMUSH_PAIR), None);
+        assert_eq!(smush_rule_4('(', '}', figfont::SMUSH_PAIR), None);
+        assert_eq!(smush_rule_4('(', ']', figfont::SMUSH_PAIR), None);
+        assert_eq!(smush_rule_4('(', '(', figfont::SMUSH_PAIR), None);
+    }
 
-        // Rule 5: BIG X SMUSHING (code value 16)
-        // Smushes "/\" into "|", "\/" into "Y", and "><" into "X". Note that "<>" is not
-        // smushed in any way by this rule. The name "BIG X" is historical; originally all
-        // three pairs were smushed into "X".
-        if self.mode & figfont::SMUSH_BIGX != 0 {
-            cmp_return!('/', '\\', l, r, '|');
-            cmp_return!('\\', '/', l, r, 'Y');
-            cmp_return!('>', '<', l, r, 'X');
-        }
-
-        return None
+    #[test]
+    fn test_rule_5() {
+        assert_eq!(smush_rule_5('/', '\\', 0), None);
+        assert_eq!(smush_rule_5('\\', '/', 0), None);
+        assert_eq!(smush_rule_5('>', '<', 0), None);
+        assert_eq!(smush_rule_5('<', '>', 0), None);
+        assert_eq!(smush_rule_5('/', '\\', figfont::SMUSH_BIGX), Some('|'));
+        assert_eq!(smush_rule_5('\\', '/', figfont::SMUSH_BIGX), Some('Y'));
+        assert_eq!(smush_rule_5('>', '<', figfont::SMUSH_BIGX), Some('X'));
+        assert_eq!(smush_rule_5('<', '>', figfont::SMUSH_BIGX), None);
     }
 }
