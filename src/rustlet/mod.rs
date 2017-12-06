607203199adcd985fb30cb2f61585335cd3bee8c
Claudio Matsuoka <cmatsuoka@gmail.com>
2017-12-06 09:26:41 -0200

    Refactor: spin off module for str smushing
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/rustlet/mod.rs
+++ src/rustlet/mod.rs
@@ -3,13 +3,12 @@ use std::error::Error;
 use self::figfont::{FIGchar, FIGfont};
 
 pub mod figfont;
-pub mod charsmush;
+mod charsmush;
+mod strsmush;
 
 
 #[derive(Debug)]
 pub struct Smusher<'a> {
-    prev_width: isize,
-    curr_width: isize,
     mode      : u32,
     right2left: bool,
     font      : &'a FIGfont,
@@ -22,8 +21,6 @@ impl<'a> Smusher<'a> {
     pub fn new(font: &'a FIGfont) -> Self {
         let mut sm = Smusher{
             font,
-            prev_width: 0,
-            curr_width: 0,
             mode      : font.layout,
             right2left: false,
             output    : Vec::new(),
@@ -76,7 +73,7 @@ impl<'a> Smusher<'a> {
 fn smusher_amount(output: &Vec<String>, c: &FIGchar, hardblank: char, mode: u32) -> usize {
     let mut amt = 9999;
     for i in 0..output.len() {
-        amt = min(amt, output[i].smush_amount(&c.lines[i], hardblank, mode));
+        amt = min(amt, strsmush::amount(&output[i], &c.lines[i], hardblank, mode));
     }
     amt
 }
@@ -88,98 +85,7 @@ println!("> amt={}", amt);
     let mut res = Vec::new();
 
     for i in 0..output.len() {
-        res.push(smush_str(&output[i], &fc.lines[i], amt, hardblank, false, mode)?);
-    }
-
-    Ok(res)
-}
-
-trait Smush {
-    fn smush_amount(self, &str, char, u32) -> usize;
-}
-
-impl<'a> Smush for &'a str {
-
-    // Compute the number of characters a string can be smushed into another string.
-    fn smush_amount(self, s: &str, hardblank: char, mode: u32) -> usize {
-    
-        let a1 = self.len() - match self.rfind(|x| { let y:char = x; !y.is_whitespace() }) {
-            Some(val) => val + 1,
-            None      => 0,
-        };
-    
-        let a2 = match s.find(|x| { let y:char = x; !y.is_whitespace() }) {
-            Some(val) => val,
-            None      => s.len(),
-        };
-
-println!(">> s1={:?} s2={:?} a1={} a2={}", self, s, a1, a2);
-        let amt = a1 + a2;
-
-        // Retrieve character pair and see if they're smushable
-        let (l, r) = match smush_chars(self, s, amt + 1) {
-            Some(pair) => pair,
-            None       => { return amt; }
-        };
-        match charsmush::smush(l, r, hardblank, false, mode) {
-            Some(_) => { amt + 1 },
-            None    => { amt },
-        }
-    }
-}
-
-fn smush_chars(s1: &str, s2: &str, amt: usize) -> Option<(char, char)> {
-    if s1.len() == 0 || s2.len() == 0 {
-        return None;
-    }
-
-    let overlap = min(amt, s2.len());
-
-    for i in (0..overlap).rev() {
-        if s1.len() + i < amt {
-            return None;
-        }
-        let l = s1.chars().nth(s1.len() + i - amt).unwrap();
-        let r = s2.chars().nth(i).unwrap();
-        if l != ' ' && r != ' ' {
-            return Some((l, r));
-        }
-    }
-
-    None
-}
-
-fn smush_str(s1: &str, s2: &str, mut amt: usize, hardblank: char, right2left: bool,
-             mode: u32) -> Result<String, Box<Error>> {
-
-    /*let a2 = match s2.find(|x| { let y:char = x; !y.is_whitespace() }) {
-        Some(val) => val,
-        None      => s2.len(),
-    };
-
-    let s2 = s2.trim_left();
-    if a2 > amt {
-        amt = 0;
-    } else {
-        amt -= a2;
-    }*/
-    let mut limit: usize;
-    if amt > s1.len() {
-        limit = 1;
-    } else {
-        limit = s1.len() - amt;
-    }
-    let mut res = s1[..limit].to_string();
-println!("s1={:?} s2={:?} amt={} res={:?}", s1, s2, amt, res);
-
-    let (l, r) = match smush_chars(s1, s2, amt) {
-        Some(pair) => pair,
-        None       => { res.push_str(s2); return Ok(res) },
-    };
-
-    match charsmush::smush(l, r, hardblank, false, mode) {
-        Some(c) => { res.push(c); res.push_str(&s2[1..]) },
-        None    => res += s2,
+        res.push(strsmush::smush(&output[i], &fc.lines[i], amt, hardblank, false, mode)?);
     }
 
     Ok(res)
@@ -191,67 +97,6 @@ mod tests {
 
     macro_rules! vec_of_strings {
         ( $($x:expr),* ) => (vec![$($x.to_string()),*])
-    }
-
-    #[test]
-    fn test_smush_chars() {
-        assert_eq!(smush_chars("    ", "    ", 2), None);
-        assert_eq!(smush_chars("abc ", " xyz", 1), None);
-        assert_eq!(smush_chars("abc ", " xyz", 2), None);
-        assert_eq!(smush_chars("abc ", " xyz", 3), Some(('c', 'x')));
-        assert_eq!(smush_chars("abc   ", " x", 5), Some(('c', 'x')));
-        assert_eq!(smush_chars("a ", " xyzwt", 3), Some(('a', 'x')));
-        assert_eq!(smush_chars("a", "      x", 6), None);
-        assert_eq!(smush_chars("a", "      x", 7), Some(('a', 'x')));
-        assert_eq!(smush_chars("", "       x", 7), None);
-    }
-
-    #[test]
-    fn test_smush_amount_str() {
-        assert_eq!("".smush_amount("", '$', 0xbf), 0);
-
-        assert_eq!("".smush_amount("    ", '$', 0xbf), 4);
-        assert_eq!("".smush_amount("   y", '$', 0xbf), 3);
-
-        assert_eq!("    ".smush_amount("    ", '$', 0xbf), 8);
-        assert_eq!("x   ".smush_amount("    ", '$', 0xbf), 7);
-        assert_eq!("xx  ".smush_amount("    ", '$', 0xbf), 6);
-        assert_eq!("xxx ".smush_amount("    ", '$', 0xbf), 5);
-        assert_eq!("xxxx".smush_amount("    ", '$', 0xbf), 4);
-
-        assert_eq!("    ".smush_amount("   y", '$', 0xbf), 7);
-        assert_eq!("x   ".smush_amount("   y", '$', 0xbf), 6);
-        assert_eq!("xx  ".smush_amount("   y", '$', 0xbf), 5);
-        assert_eq!("xxx ".smush_amount("   y", '$', 0xbf), 4);
-        assert_eq!("xxxx".smush_amount("   y", '$', 0xbf), 3);
-
-        assert_eq!("    ".smush_amount("  yy", '$', 0xbf), 6);
-        assert_eq!("x   ".smush_amount("  yy", '$', 0xbf), 5);
-        assert_eq!("xx  ".smush_amount("  yy", '$', 0xbf), 4);
-        assert_eq!("xxx ".smush_amount("  yy", '$', 0xbf), 3);
-        assert_eq!("xxxx".smush_amount("  yy", '$', 0xbf), 2);
-
-        assert_eq!("    ".smush_amount(" yyy", '$', 0xbf), 5);
-        assert_eq!("x   ".smush_amount(" yyy", '$', 0xbf), 4);
-        assert_eq!("xx  ".smush_amount(" yyy", '$', 0xbf), 3);
-        assert_eq!("xxx ".smush_amount(" yyy", '$', 0xbf), 2);
-        assert_eq!("xxxx".smush_amount(" yyy", '$', 0xbf), 1);
-
-        assert_eq!("    ".smush_amount("yyyy", '$', 0xbf), 4);
-        assert_eq!("x   ".smush_amount("yyyy", '$', 0xbf), 3);
-        assert_eq!("xx  ".smush_amount("yyyy", '$', 0xbf), 2);
-        assert_eq!("xxx ".smush_amount("yyyy", '$', 0xbf), 1);
-        assert_eq!("xxxx".smush_amount("yyyy", '$', 0xbf), 0);
-
-        assert_eq!("x".smush_amount("y", '$', 0xbf), 0);
-        assert_eq!("x".smush_amount("x", '$', 0xbf), 1);     // rule 1
-        assert_eq!("<".smush_amount(">", '$', 0xbf), 0);
-        assert_eq!("_".smush_amount("/", '$', 0xbf), 1);     // rule 2
-        assert_eq!("/".smush_amount("_", '$', 0xbf), 1);     // rule 2
-        assert_eq!("[".smush_amount("{", '$', 0xbf), 1);     // rule 3
-        assert_eq!("[".smush_amount("]", '$', 0xbf), 1);     // rule 4
-        assert_eq!(">".smush_amount("<", '$', 0xbf), 1);     // rule 5
-        assert_eq!("[ ".smush_amount(" {", '$', 0xbf), 3);   // rule 3 + spacing
     }
 
     #[test]
@@ -271,15 +116,5 @@ mod tests {
         let output = vec_of_strings![  "xxxx ", "xxx  ", "xx   ", "x    " ];
         let fc = FIGchar{ lines: vec_of_strings![ "   x", "  xx", " xxx", "xxxx" ] };
         assert_eq!(smusher_amount(&output, &fc, '$', 0xbf), 5);
-    }
-
-    #[test]
-    fn test_smush_str() {
-        assert_eq!(smush_str("123! ", "xy", 1, '$', false, 0xbf).ok(), Some("123!xy".to_string()));
-        assert_eq!(smush_str("123> ", "<y", 2, '$', false, 0xbf).ok(), Some("123Xy".to_string()));
-        assert_eq!(smush_str("123! ", "   xy", 5, '$', false, 0xbf).ok(), Some("123xy".to_string()));
-        assert_eq!(smush_str("123/ ", "   /y", 5, '$', false, 0xbf).ok(), Some("123/y".to_string()));
-        assert_eq!(smush_str("", "   y", 3, '$', false, 0xbf).ok(), Some("y".to_string()));
-        assert_eq!(smush_str("", "      ", 1, '$', false, 0xbf).ok(), Some("".to_string()));
     }
 }
---
+++ src/rustlet/strsmush.rs
@@ -0,0 +1,170 @@
+use std::cmp::min;
+use std::error::Error;
+use super::charsmush;
+
+
+
+// Compute the number of characters a string can be smushed into another string.
+pub fn amount(s1: &str, s2: &str, hardblank: char, mode: u32) -> usize {
+
+    let a1 = s1.len() - match s1.rfind(|x| { let y:char = x; !y.is_whitespace() }) {
+        Some(val) => val + 1,
+        None      => 0,
+    };
+
+    let a2 = match s2.find(|x| { let y:char = x; !y.is_whitespace() }) {
+        Some(val) => val,
+        None      => s2.len(),
+    };
+
+println!(">> s1={:?} s2={:?} a1={} a2={}", s1, s2, a1, a2);
+    let amt = a1 + a2;
+
+    // Retrieve character pair and see if they're smushable
+    let (l, r) = match get_pair(s1, s2, amt + 1) {
+        Some(pair) => pair,
+        None       => { return amt; }
+    };
+    match charsmush::smush(l, r, hardblank, false, mode) {
+        Some(_) => { amt + 1 },
+        None    => { amt },
+    }
+}
+
+pub fn smush(s1: &str, s2: &str, mut amt: usize, hardblank: char, right2left: bool,
+             mode: u32) -> Result<String, Box<Error>> {
+
+    /*let a2 = match s2.find(|x| { let y:char = x; !y.is_whitespace() }) {
+        Some(val) => val,
+        None      => s2.len(),
+    };
+
+    let s2 = s2.trim_left();
+    if a2 > amt {
+        amt = 0;
+    } else {
+        amt -= a2;
+    }*/
+    let mut limit: usize;
+    if amt > s1.len() {
+        limit = 1;
+    } else {
+        limit = s1.len() - amt;
+    }
+    let mut res = s1[..limit].to_string();
+println!("s1={:?} s2={:?} amt={} res={:?}", s1, s2, amt, res);
+
+    let (l, r) = match get_pair(s1, s2, amt) {
+        Some(pair) => pair,
+        None       => { res.push_str(s2); return Ok(res) },
+    };
+
+    match charsmush::smush(l, r, hardblank, false, mode) {
+        Some(c) => { res.push(c); res.push_str(&s2[1..]) },
+        None    => res += s2,
+    }
+
+    Ok(res)
+}
+
+fn get_pair(s1: &str, s2: &str, amt: usize) -> Option<(char, char)> {
+    if s1.len() == 0 || s2.len() == 0 {
+        return None;
+    }
+
+    let overlap = min(amt, s2.len());
+
+    for i in (0..overlap).rev() {
+        if s1.len() + i < amt {
+            return None;
+        }
+        let l = s1.chars().nth(s1.len() + i - amt).unwrap();
+        let r = s2.chars().nth(i).unwrap();
+        if l != ' ' && r != ' ' {
+            return Some((l, r));
+        }
+    }
+
+    None
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! vec_of_strings {
+        ( $($x:expr),* ) => (vec![$($x.to_string()),*])
+    }
+
+    #[test]
+    fn test_get_pair() {
+        assert_eq!(get_pair("    ", "    ", 2), None);
+        assert_eq!(get_pair("abc ", " xyz", 1), None);
+        assert_eq!(get_pair("abc ", " xyz", 2), None);
+        assert_eq!(get_pair("abc ", " xyz", 3), Some(('c', 'x')));
+        assert_eq!(get_pair("abc   ", " x", 5), Some(('c', 'x')));
+        assert_eq!(get_pair("a ", " xyzwt", 3), Some(('a', 'x')));
+        assert_eq!(get_pair("a", "      x", 6), None);
+        assert_eq!(get_pair("a", "      x", 7), Some(('a', 'x')));
+        assert_eq!(get_pair("", "       x", 7), None);
+    }
+
+    #[test]
+    fn test_amount() {
+        assert_eq!(amount("", "", '$', 0xbf), 0);
+
+        assert_eq!(amount("", "    ", '$', 0xbf), 4);
+        assert_eq!(amount("", "   y", '$', 0xbf), 3);
+
+        assert_eq!(amount("    ", "    ", '$', 0xbf), 8);
+        assert_eq!(amount("x   ", "    ", '$', 0xbf), 7);
+        assert_eq!(amount("xx  ", "    ", '$', 0xbf), 6);
+        assert_eq!(amount("xxx ", "    ", '$', 0xbf), 5);
+        assert_eq!(amount("XXXX", "    ", '$', 0xbf), 4);
+
+        assert_eq!(amount("    ", "   y", '$', 0xbf), 7);
+        assert_eq!(amount("x   ", "   y", '$', 0xbf), 6);
+        assert_eq!(amount("xx  ", "   y", '$', 0xbf), 5);
+        assert_eq!(amount("xxx ", "   y", '$', 0xbf), 4);
+        assert_eq!(amount("xxxx", "   y", '$', 0xbf), 3);
+
+        assert_eq!(amount("    ", "  yy", '$', 0xbf), 6);
+        assert_eq!(amount("x   ", "  yy", '$', 0xbf), 5);
+        assert_eq!(amount("xx  ", "  yy", '$', 0xbf), 4);
+        assert_eq!(amount("xxx ", "  yy", '$', 0xbf), 3);
+        assert_eq!(amount("xxxx", "  yy", '$', 0xbf), 2);
+
+        assert_eq!(amount("    ", " yyy", '$', 0xbf), 5);
+        assert_eq!(amount("x   ", " yyy", '$', 0xbf), 4);
+        assert_eq!(amount("xx  ", " yyy", '$', 0xbf), 3);
+        assert_eq!(amount("xxx ", " yyy", '$', 0xbf), 2);
+        assert_eq!(amount("xxxx", " yyy", '$', 0xbf), 1);
+
+        assert_eq!(amount("    ", "yyyy", '$', 0xbf), 4);
+        assert_eq!(amount("x   ", "yyyy", '$', 0xbf), 3);
+        assert_eq!(amount("xx  ", "yyyy", '$', 0xbf), 2);
+        assert_eq!(amount("xxx ", "yyyy", '$', 0xbf), 1);
+        assert_eq!(amount("xxxx", "yyyy", '$', 0xbf), 0);
+
+        assert_eq!(amount("x", "y", '$', 0xbf), 0);
+        assert_eq!(amount("x", "x", '$', 0xbf), 1);     // rule 1
+        assert_eq!(amount("<", ">", '$', 0xbf), 0);
+        assert_eq!(amount("_", "/", '$', 0xbf), 1);     // rule 2
+        assert_eq!(amount("/", "_", '$', 0xbf), 1);     // rule 2
+        assert_eq!(amount("[", "{", '$', 0xbf), 1);     // rule 3
+        assert_eq!(amount("[", "]", '$', 0xbf), 1);     // rule 4
+        assert_eq!(amount(">", "<", '$', 0xbf), 1);     // rule 5
+        assert_eq!(amount("[ ", " {", '$', 0xbf), 3);   // rule 3 + spacing
+    }
+
+    #[test]
+    fn test_smush() {
+        assert_eq!(smush("123! ", "xy", 1, '$', false, 0xbf).ok(), Some("123!xy".to_string()));
+        assert_eq!(smush("123> ", "<y", 2, '$', false, 0xbf).ok(), Some("123Xy".to_string()));
+        assert_eq!(smush("123! ", "   xy", 5, '$', false, 0xbf).ok(), Some("123xy".to_string()));
+        assert_eq!(smush("123/ ", "   /y", 5, '$', false, 0xbf).ok(), Some("123/y".to_string()));
+        assert_eq!(smush("", "   y", 3, '$', false, 0xbf).ok(), Some("y".to_string()));
+        assert_eq!(smush("", "      ", 1, '$', false, 0xbf).ok(), Some("".to_string()));
+    }
+}
