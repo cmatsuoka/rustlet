b892887075f4c69e4c42c7f92499c9146dce7f11
Claudio Matsuoka <cmatsuoka@gmail.com>
2017-12-27 20:44:37 -0200

    Add showfonts example
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

---
+++ examples/showfonts.rs
@@ -0,0 +1,32 @@
+extern crate rustlet;
+
+use std::error::Error;
+use std::fs;
+use std::path::PathBuf;
+use rustlet::{FIGfont, Smusher};
+
+fn main() {
+    match run() {
+        Ok(_)  => {},
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
+fn run() -> Result<(), Box<Error>> {
+    let path = env!("CARGO_MANIFEST_DIR").to_owned() + "/fonts";
+    for f in fs::read_dir(&path)? {
+        show_font(f?.path(), &path)?;
+    }
+    Ok(())
+}
+
+fn show_font(p: PathBuf, prefix: &str) -> Result<(), Box<Error>> {
+    let font = FIGfont::from_file(p.to_str().unwrap())?;
+    let name = p.strip_prefix(prefix)?.file_stem().unwrap().to_str().unwrap();
+    println!("{}:", name); 
+    let mut sm = Smusher::new(&font);
+    sm.push_str(name);
+    sm.get().iter().for_each(|x| println!("{}", x));
+    println!();
+    Ok(())
+}
